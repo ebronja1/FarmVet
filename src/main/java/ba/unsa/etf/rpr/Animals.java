aca3e80a88476e24abb47a9908012e5939be45dd
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-10 21:51:52 +0100

    Dao class needed for work with database

--- src/main/java/ba/unsa/etf/rpr/Animals.java
+++ src/main/java/ba/unsa/etf/rpr/Animals.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr;
 
 public class Animals {
+    private int id;
+    private String name;
+    private String kind;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
 }
