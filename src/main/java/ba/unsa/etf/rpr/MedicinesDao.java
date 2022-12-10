c339b345f5a763bba78731871b4a1f7561854e4d
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-10 23:33:06 +0100

    MedicineDao class for Medicine bean

--- src/main/java/ba/unsa/etf/rpr/MedicinesDao.java
+++ src/main/java/ba/unsa/etf/rpr/MedicinesDao.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr;
 
-public interface MedicinesDao {
+
+import ba.unsa.etf.rpr.Vets;
+
+import java.util.List;
+
+/**
+ * Dao interface for Medicines bean
+ *
+ * @author Emir Bronja
+ */
+public interface MedicinesDao extends Dao<Medicines> {
+    /**
+     * Returns all medicines that contains given text.
+     *
+     * @param animal search string for quotes
+     * @return list of quotes
+     */
+    List<Medicines> searchByCategory(Animals animal);
+    /**
+     * Returns all medicines that contains given text.
+     *
+     * @param vet search string for quotes
+     * @return list of quotes
+     */
+    List<Medicines> searchByCategory(Vets vet);
 }
