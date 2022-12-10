f4ab4a4eacd394f600c29c6c5b08dc201b09ef58
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-10 22:27:01 +0100

    Dao class needed for work with database

--- src/main/java/ba/unsa/etf/rpr/Medicines.java
+++ src/main/java/ba/unsa/etf/rpr/Medicines.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr;
 
 public class Medicines {
+    private int id;
+    private String name;
+    private Animals animals;
+    private Vets vets;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Animals getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(Animals animals) {
+        this.animals = animals;
+    }
+
+    public Vets getVets() {
+        return vets;
+    }
+
+    public void setVets(Vets vets) {
+        this.vets = vets;
+    }
 }
