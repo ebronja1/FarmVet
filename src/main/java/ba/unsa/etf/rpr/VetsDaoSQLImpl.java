4ba9f6ffe56abbebf8066761c25b5dad56e18e89
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-11 11:44:20 +0100

    Implementation of VetsDao

--- src/main/java/ba/unsa/etf/rpr/VetsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/VetsDaoSQLImpl.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr;
 
-public class VetsDaoSQLImpl {
+import ba.unsa.etf.rpr.Vets;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+public class VetsDaoSQLImpl implements VetsDao{
+    private Connection connection;
+
+    public VetsDaoSQLImpl(){
+        try{
+            this.connection = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7582258", "sql7582258", "m6JTHrdhjw");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Vets getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Vets add(Vets item) {
+        return null;
+    }
+
+    @Override
+    public Vets update(Vets item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Vets> getAll() {
+        return null;
+    }
 }
