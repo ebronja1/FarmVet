2481ca7ce71e1bf8fbba693b3d3287e9080379d1
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-11 13:18:04 +0100

    Implementation of AnimalsDao

--- src/main/java/ba/unsa/etf/rpr/AnimalsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/AnimalsDaoSQLImpl.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr;
 
-public class AnimalsDaoSQLImpl {
+import ba.unsa.etf.rpr.Animals;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalsDaoSQLImpl implements AnimalsDao {
+    private Connection connection;
+
+    public AnimalsDaoSQLImpl(){
+        try{
+            this.connection = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7582258", "sql7582258", "m6JTHrdhjw");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public Animals getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Animals add(Animals item) {
+        return null;
+    }
+
+    @Override
+    public Animals update(Animals item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Animals> getAll() {
+        return null;
+    }
 }
