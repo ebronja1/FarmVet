8d2857489fcaf0d27fb141738fd938ed3c7e185a
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-17 18:48:40 +0100

    Starting with graphic interface

--- src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
@@ -1,8 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class SampleController {
+    public Button okdugmeid;
+    public TextField usernameid;
+
     public void nekaakcija(ActionEvent actionEvent) {
+        usernameid.setText("Please type username");
     }
 }
