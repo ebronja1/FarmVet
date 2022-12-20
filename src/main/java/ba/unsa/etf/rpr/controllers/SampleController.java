5f1fc312c0130b609e7ffc8c8683d96688abca4b
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-20 21:54:27 +0100

    Graphic interface

--- src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -8,6 +11,23 @@ import javafx.scene.control.TextField;
 public class SampleController {
     public Button okdugmeid;
     public TextField usernameid;
+
+    @FXML
+    public void initialize() {
+        usernameid.getStyleClass().add("poljeNijeIspravno");
+        usernameid.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (n.trim().isEmpty()) {
+                    usernameid.getStyleClass().removeAll("poljeJeIspravno");
+                    usernameid.getStyleClass().add("poljeNijeIspravno");
+                } else {
+                    usernameid.getStyleClass().removeAll("poljeNijeIspravno");
+                    usernameid.getStyleClass().add("poljeJeIspravno");
+                }
+            }
+        });
+    }
 
     public void okButtonClick(ActionEvent actionEvent) {
         if (usernameid.getText().isEmpty()) {
