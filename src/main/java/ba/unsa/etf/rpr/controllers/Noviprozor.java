a845a9cb654b8813496051df2e74b79d319df14f
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-24 18:40:10 +0100

    class tbButton

--- src/main/java/ba/unsa/etf/rpr/controllers/Noviprozor.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Noviprozor.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -11,8 +14,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class Noviprozor {
     public TextArea textArea;
+    public Label statusBar;
 
     public void akcijaKraj(ActionEvent actionEvent) {
         System.exit(0);
@@ -28,6 +34,7 @@ public class Noviprozor {
         try {
             String tekst = new String(Files.readAllBytes(izabrani.toPath()));
             textArea.setText(tekst);
+            statusBar.setText("Datoteka je ucitana!");
         } catch (IOException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText("Greska");
@@ -35,5 +42,17 @@ public class Noviprozor {
             alert.setTitle("Ne mogu da otvorim datoteku");
             alert.showAndWait();
         }
+    }
+
+    public void akcijaOpenNewWindow(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/properties.fxml"));
+        Parent root = loader.load();
+        Noviprozor noviprozor = loader.getController();
+        //noviprozor.dobrodosaoButtonid.setText(noviprozor.dobrodosaoButtonid.getText() + usernameid.getText());
+        stage.setTitle("Properties");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
 }
