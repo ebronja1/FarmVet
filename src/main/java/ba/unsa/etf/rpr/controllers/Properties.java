e37ef9911fed08b531596d868e0d9cf2171bb487
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-24 22:22:01 +0100

    new window properties spinner cancel button action

--- src/main/java/ba/unsa/etf/rpr/controllers/Properties.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Properties.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Properties {
+    public Button buttonCancelProperties;
+
+    public void actionClose(ActionEvent actionEvent) {
+        Stage stage = (Stage)buttonCancelProperties.getScene().getWindow();
+        stage.close();
+    }
 }
