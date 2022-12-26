5a7e8e040b2b3f4f33c09fe18110ea4d8e8a6c7b
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-26 14:59:43 +0100

    Abstract dao new class prepareInsertParts method

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -64,6 +64,29 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
             throw new FarmVetException(e.getMessage(), e);
         }
     }
+
+    /**
+     * Accepts KV storage of column names and return CSV of columns and question marks for insert statement
+     * Example: (id, name, date) ?,?,?
+     */
+    private Map.Entry<String, String> prepareInsertParts(Map<String, Object> row){
+        StringBuilder columns = new StringBuilder();
+        StringBuilder questions = new StringBuilder();
+
+        int counter = 0;
+        for (Map.Entry<String, Object> entry: row.entrySet()) {
+            counter++;
+            if (entry.getKey().equals("id")) continue; //skip insertion of id due autoincrement
+            columns.append(entry.getKey());
+            questions.append("?");
+            if (row.size() != counter) {
+                columns.append(",");
+                questions.append(",");
+            }
+        }
+        return new AbstractMap.SimpleEntry<>(columns.toString(), questions.toString());
+    }
+
     public List<T> executeQuery(String query, Object[] params) throws FarmVetException{
         try {
             PreparedStatement stmt = getConnection().prepareStatement(query);
