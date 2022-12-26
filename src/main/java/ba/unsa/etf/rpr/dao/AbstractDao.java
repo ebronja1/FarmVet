9526967aed6138471e3c1bfe0ea4c66f28df8229
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-26 15:04:20 +0100

    Abstract dao new class prepareUpdateParts method

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -65,27 +65,6 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
         }
     }
 
-    /**
-     * Accepts KV storage of column names and return CSV of columns and question marks for insert statement
-     * Example: (id, name, date) ?,?,?
-     */
-    private Map.Entry<String, String> prepareInsertParts(Map<String, Object> row){
-        StringBuilder columns = new StringBuilder();
-        StringBuilder questions = new StringBuilder();
-
-        int counter = 0;
-        for (Map.Entry<String, Object> entry: row.entrySet()) {
-            counter++;
-            if (entry.getKey().equals("id")) continue; //skip insertion of id due autoincrement
-            columns.append(entry.getKey());
-            questions.append("?");
-            if (row.size() != counter) {
-                columns.append(",");
-                questions.append(",");
-            }
-        }
-        return new AbstractMap.SimpleEntry<>(columns.toString(), questions.toString());
-    }
 
     public List<T> executeQuery(String query, Object[] params) throws FarmVetException{
         try {
@@ -120,5 +99,42 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
             throw new FarmVetException("Object not found");
         }
     }
+    private Map.Entry<String, String> prepareInsertParts(Map<String, Object> row){
+        StringBuilder columns = new StringBuilder();
+        StringBuilder questions = new StringBuilder();
+
+        int counter = 0;
+        for (Map.Entry<String, Object> entry: row.entrySet()) {
+            counter++;
+            if (entry.getKey().equals("id")) continue; //skip insertion of id due autoincrement
+            columns.append(entry.getKey());
+            questions.append("?");
+            if (row.size() != counter) {
+                columns.append(",");
+                questions.append(",");
+            }
+        }
+        return new AbstractMap.SimpleEntry<>(columns.toString(), questions.toString());
+    }
+    /**
+     * Prepare columns for update statement id=?, name=?, ...
+     * @param row - row to be converted intro string
+     * @return String for update statement
+     */
+    private String prepareUpdateParts(Map<String, Object> row){
+        StringBuilder columns = new StringBuilder();
+
+        int counter = 0;
+        for (Map.Entry<String, Object> entry: row.entrySet()) {
+            counter++;
+            if (entry.getKey().equals("id")) continue; //skip update of id due where clause
+            columns.append(entry.getKey()).append("= ?");
+            if (row.size() != counter) {
+                columns.append(",");
+            }
+        }
+        return columns.toString();
+    }
 }
+
 
