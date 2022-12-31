9d2e14858ee7a70f241d21883cad49db92d73587
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-31 20:24:47 +0100

    Abstract dao new class update method  create connection for singletone pattern

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -12,21 +12,26 @@ import java.util.*;
  * @author Emir Bronja
  */
 public abstract class AbstractDao<T extends Idable> implements Dao<T>{
-    private Connection connection;
+    private static Connection connection = null;
     private String tableName;
 
     public AbstractDao(String tableName) {
-        try{
-            this.tableName = tableName;
-            Properties p = new Properties();
-            p.load(ClassLoader.getSystemResource("application.properties").openStream());
-            String url = p.getProperty("db.url");
-            String username = p.getProperty("db.user");
-            String password = p.getProperty("db.password");
-            this.connection = DriverManager.getConnection(url, username, password);
-        }catch (Exception e){
-            e.printStackTrace();
-            System.exit(0);
+        this.tableName = tableName;
+        if(connection==null) createConnection();
+    }
+    private static void createConnection() {
+        if (AbstractDao.connection == null) {
+            try {
+                Properties p = new Properties();
+                p.load(ClassLoader.getSystemResource("application.properties").openStream());
+                String url = p.getProperty("db.url");
+                String username = p.getProperty("db.user");
+                String password = p.getProperty("db.password");
+                AbstractDao.connection = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
         }
     }
     public Connection getConnection(){
