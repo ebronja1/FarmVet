35a21131ed03b66259a6c283afdf743420841809
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-31 21:02:12 +0100

    DaoFactory class for singletone pattern DaoFActory method

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.dao;
 
+import java.sql.Connection;
+
+/**
+ * Factory method for singleton implementation of DAOs
+ *
+ * @author Emir Bronja
+ */
 public class DaoFactory {
+
+    private static final AnimalsDao categoryDao = new AnimalsDaoSQLImpl();
+    private static final MedicinesDao quoteDao = new MedicinesDaoSQLImpl();
+    private static final VetsDao quoteHistoryDao = new VetsDaoSQLImpl();
+
+
+    private DaoFactory() {
+    }
 }
