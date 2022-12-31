617f0b67fca1d975ad72ed657d6bad62e5c5fb50
ebronja1 <ebronja1@etf.unsa.ba>
2022-12-31 20:43:24 +0100

    Abstract dao new class update method   setConnection for singletone pattern

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -37,8 +37,16 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
     public static Connection getConnection(){
         return AbstractDao.connection;
     }
+
     public void setConnection(Connection connection){
-        this.connection = connection;
+        if(AbstractDao.connection!=null) {
+            try {
+                AbstractDao.connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        AbstractDao.connection = connection;
     }
     /**
      * Method for mapping ResultSet into Object
