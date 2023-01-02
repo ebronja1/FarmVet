1362a68e9d7f878860266db6b724fff72d93f1d4
ebronja1 <ebronja1@etf.unsa.ba>
2023-01-02 13:37:30 +0100

    busines class VetsManager validateVetsName method

--- src/main/java/ba/unsa/etf/rpr/business/VetsManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/VetsManager.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.business;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Animals;
+import ba.unsa.etf.rpr.domain.Vets;
+import ba.unsa.etf.rpr.exceptions.FarmVetException;
+
+import java.util.List;
+
+/**
+ * Business Logic Layer for management of Medicines
+ *
+ * @author Emir Bronja
+ */
 
 public class VetsManager {
+    public void validateVetsName(String name) throws FarmVetException {
+        if (name == null || name.length() > 45 || name.length() < 3) {
+            throw new FarmVetException("Vet's name must be between 3 and 45 chars");
+        }
+    }
 }
