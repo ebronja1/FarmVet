730a7f100023af45eb8b637bcebf8bcd4954624f
ebronja1 <ebronja1@etf.unsa.ba>
2023-01-02 12:58:35 +0100

    busines class MedicinesManager getAll method

--- src/main/java/ba/unsa/etf/rpr/business/MedicinesManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/MedicinesManager.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.business;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Medicines;
+import ba.unsa.etf.rpr.domain.Vets;
+import ba.unsa.etf.rpr.domain.Animals;
+import ba.unsa.etf.rpr.exceptions.FarmVetException;
 
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Business Logic Layer for Medicines
+ *
+ * @author Emir Bronja
+ */
 public class MedicinesManager {
+    public List<Medicines> getAll() throws FarmVetException {
+        return DaoFactory.medicinesDao().getAll();
+    }
 }
