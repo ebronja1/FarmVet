7c7b4c4d3baa1072b3f74a0c3542aae0a5c8f000
ebronja1 <ebronja1@etf.unsa.ba>
2023-01-02 13:12:34 +0100

    busines class AnimalsManager validateAnimalsName method

--- src/main/java/ba/unsa/etf/rpr/business/MedicinesManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/MedicinesManager.java
@@ -1,11 +1,8 @@
 package ba.unsa.etf.rpr.business;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Medicines;
-import ba.unsa.etf.rpr.domain.Vets;
-import ba.unsa.etf.rpr.domain.Animals;
 import ba.unsa.etf.rpr.exceptions.FarmVetException;
 
-import java.util.Date;
 import java.util.List;
 
 /**
