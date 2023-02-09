3ea1a5d4278a0d3787b52dfab64a0dfeed257a75
ebronja1 <ebronja1@etf.unsa.ba>
2023-02-09 12:04:34 +0100

    printFormattedOptions method

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr;
+import net.bytebuddy.asm.Advice;
+import org.apache.commons.cli.*;
+import java.sql.Date;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Stream;
 
+/**
+ * @author Emir Bronja
+ * CLI (Command Line Interface) implementation in following class
+ * Even though this type of presentation layer (called CLI) is becoming past tense for GUI apps
+ * it's good to see how you can manipulate data through command line and database also
+ *
+ */
 public class App {
+    /**
+     *
+     * @param options
+     *
+     */
+    public static void printFormattedOptions(Options options) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        PrintWriter printWriter = new PrintWriter(System.out);
+        helpFormatter.printUsage(printWriter, 150, "java -jar quote-maker.jar [option] 'something else if needed' ");
+        helpFormatter.printOptions(printWriter, 150, options, 2, 7);
+        printWriter.close();
+    }
 }
