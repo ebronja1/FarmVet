404131f35c7f16ff0c7d22118db20a918089eaec
ebronja1 <ebronja1@etf.unsa.ba>
2023-02-11 10:55:08 +0100

    Test adding new animal with id that's not zero

--- src/test/java/ba/unsa/etf/rpr/business/AnimalsManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/AnimalsManagerTest.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.domain.Animals;
+import ba.unsa.etf.rpr.exceptions.FarmVetException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class AnimalsManagerTest {
+    AnimalsManager animalsManager = new AnimalsManager();
+    @Test
+    void TestAddingWithId() {
+        Animals animal = new Animals();
+        animal.setName("Kiki");
+        animal.setKind("Cat");
+        animal.setId(1);
+        assertThrows(FarmVetException.class, () -> {
+           animalsManager.add(animal);
+        });
+    }
 }
